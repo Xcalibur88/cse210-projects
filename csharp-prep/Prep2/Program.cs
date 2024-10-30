ffa407c1ea4452fd6f2888538a29efaee82a1608
Elijah Lord <81175247+Xcalibur88@users.noreply.github.com>
2024-10-29 18:38:11 -0600

    Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,44 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+class Program {
+    static void Main(string[] args) {
+        Console.Write("What is your percentage grade: ");
+        int percentageGrade = int.Parse(Console.ReadLine());
+
+        NotifyGrade(percentageGrade, GetLetterGrade(percentageGrade));
+    }
+
+    static string GetLetterGrade(int percentageGrade) {
+        string letter = "";
+        if (percentageGrade >= 90) {
+            letter = "A";
+            if (percentageGrade >= 97) {
+                return letter;
+            }
+        } else if (percentageGrade >= 80) {
+            letter = "B";
+        } else if (percentageGrade >= 70) {
+            letter = "C";
+        } else if (percentageGrade >= 60) {
+            letter = "D";
+        } else {
+            return "F";
+        }
+
+        if (percentageGrade % 10 >= 7) {
+            return letter + "+";
+        } else if (percentageGrade % 10 <= 3) {
+            return letter + "-";
+        }
+        return letter;
+    }
+
+    static void NotifyGrade(int percentageGrade, string letterGrade) {
+         Console.WriteLine($"\nYour grade: {letterGrade}");
+         if (percentageGrade >= 70) {
+            Console.WriteLine("Good work, you passed!");
+         } else {
+            Console.WriteLine("Non-passing grade. Better luck next time!");
+         }
     }
 }
